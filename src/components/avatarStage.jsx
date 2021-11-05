9ad0078330c77fc923db4df6c0fbd015b88c58a8
Seungju Ham <hsj1514@kaist.ac.kr>
2021-11-05 18:13:03 +0900

    [feature] create avatarStage component

---
+++ src/components/avatarStage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Layout = styled.div`
+height: 200px;
+background-color: skyblue;
+`;
+
+const AvatarStage = () => {
+    return (
+        <Layout>Avatar Stage</Layout>
+    );
+};
+
+export default AvatarStage;
