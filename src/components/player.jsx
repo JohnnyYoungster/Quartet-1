72c5973b29c9643b024c8cb047dcefaec00312c4
Seungju Ham <hsj1514@kaist.ac.kr>
2021-11-08 23:53:49 +0900

    [refactor] player component to functional component

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,70 +1,39 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo, useEffect } from 'react';
 import styled from 'styled-components';
 import mp3_file from './sample.mp3';
-
 
 const Layout = styled.div`
 height: 200px;
 background-color: yellow;
 `;
-// const tracks = [
-//     {
-//       title: string,
-//       artist: string,
-//       audioSrc: string | import,
-//           image: string,
-//       color: string,
-//     },
-//     ...
-//     ...
-//   ];
-
-// const audioElement = new Audio(audio source);
-class Music extends React.Component {
-    constructor(props) {
-    super(props);
-    this.state = {
-      play: false,
-      pause: true,
-    }
-    // this.url = "http://streaming.tdiradio.com:8000/house.mp3";
-    // this.src="sample.mp3";
-    this.audio = new Audio(mp3_file);
-    // this.src="";
-    // this.audio=new Audio(this.url);
-  }
-
-  play = () => {
-  this.setState({ play: true, pause: false })
-    this.audio.play();
-  }
-  
-  pause = () => {
-  this.setState({ play: false, pause: true })
-    this.audio.pause();
-  }
-  
-  render() {
-    
-  return (
-    <div>
-      <button onClick={this.play}>Play</button>
-      <button onClick={this.pause}>Pause</button>
-    </div>
-    );
-  }
-}
-
-// ReactDOM.render(
-//   <Music />,
-//   document.getElementById('container')
-// );
 
 const Player = () => {
+	const [ isPlaying, setIsPlaying ] = useState(false);
+	const audio = useMemo(() => new Audio(mp3_file), []);
+
+	const play = useCallback(() => {
+		setIsPlaying(true);
+	}, []);
+
+	const pause = useCallback(() => {
+		setIsPlaying(false);
+	}, []);
+
+	useEffect(() => {
+		if (isPlaying) {
+			audio.play();
+		} else {
+			audio.pause();
+		}
+	}, [ isPlaying ]);
+
     return (
-        <Layout>Player
-            <Music> </Music>
+        <Layout>
+			<div>Player</div>
+			<button onClick={play}>Play</button>
+			<button onClick={pause}>Pause</button>
         </Layout>
     );
 };
+
 export default Player;
