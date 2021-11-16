b2d6d06760e313a58c7ac54525a11f0a9b7f231f
YoungsterJohnny <yohanme@kaist.ac.kr>
2021-11-16 20:57:56 +0900

    (feature) backend server integration

--- src/login.js
+++ src/login.js
@@ -2,32 +2,49 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './login.css';
 
-// async function loginUser(credentials) {
-//  return fetch('http://13.209.255.184:8090', {
-//    method: 'POST',
-//    headers: {
-//      'Content-Type': 'application/json'
-//    },
-//    body: JSON.stringify(credentials)
-//  })
-//    .then(data => data.json())
-// }
+async function loginUser(credentials) {
+  // 13.209.255.184
+  // 172.31.19.57
+ return fetch('http://13.209.255.184:50002/login', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(credentials)
+ })
+   .then(data => data.json())
+}
 
+// const proxy = require("http-proxy-middleware");
+// module.exports = function (app) {
+
+//     app.use(proxy("/api", {
+//         target: "http://13.209.255.184:8090/login,", 
+//         changeOrigin:true,
+//         ws: true,
+//         router: {
+//             'localhost:3000': 'http://localhost:3001',
+//         }
+//     }));
+
+// }
 export default function Login({ setToken, setAudience}) {
   const [username, setUserName] = useState();
   // const [password, setPassword] = useState();
   const handleSubmitPerformer = async e => {
     e.preventDefault();
-    // const token = await loginUser({
-    //   username,
-    //   isPerformer
-    // });
+    const token = await loginUser({
+      username
+    });
     setToken(1);
     setAudience(false);
   }
 
   const handleSubmitAudience = async e => {
     e.preventDefault();
+    const token = await loginUser({
+      username
+    });
     setToken(1);
     setAudience(true);
   }
