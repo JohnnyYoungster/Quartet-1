4f3d8ac28773c7a8df0a2e1c9dcd0b28171b9a79
YoungsterJohnny <yohanme@kaist.ac.kr>
2021-11-16 18:49:10 +0900

    (feat) allign announcement bar box in center

--- src/components/bottombar/AnnounceBar.jsx
+++ src/components/bottombar/AnnounceBar.jsx
@@ -11,7 +11,20 @@ const AnnounceBar=({setAnnounce})=>{
 
     return (
         <div className = "announceBar">
-            <input type="textInput" onChange={e => setMsg(e.target.value)} onSubmit={onSubmit} />
+            
+            <div className="textWrapper">
+            <input type="textInput" onChange={e => setMsg(e.target.value)} onSubmit={onSubmit}
+            placeholder='Communicate with your audience'
+            style={{
+                backgroundColor: 'white',
+                borderWidth: 1,
+                borderRadius: 15,
+                width: 900,
+                height: 30,
+                fontFamily: 'Roboto',
+                fontSize: 20,
+                textAlign: 'center',
+            }}/></div>
             <img className="megaphone" src={megaphone} alt="megaphone" onClick={onSubmit} />
         </div>
     )
