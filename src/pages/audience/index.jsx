647f640ff17d74b158cafad7b8191a7823ebdaa6
Jinhyen Kim <jinhyenkim01@kaist.ac.kr>
2021-11-18 03:51:28 +0900

    Added scores to pop up at cursor location (requires GSAP)

--- src/pages/audience/index.jsx
+++ src/pages/audience/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './index.css';
 import clapImg from '../../image/clap.png';
 import clap from '../../components/resources/clap.mp3'
+import gsap from 'gsap'
 
 var decreaseFlag;
 var score = 0;
@@ -120,6 +121,7 @@ function initCloseButton() {
 }
 
 function initButton() {
+  document.getElementById("box").addEventListener("click", printMousePos);
   document.getElementById("box").onclick = function()
   {
     clap_audio.play();
@@ -156,6 +158,57 @@ function makeBox() {
 	  document.getElementById("box").style.display="block";	
   }, 0); 
 			
+}
+
+function printMousePos(event) {
+
+  var targetBorder = document.getElementById("stack");
+  var temp = parseInt(targetBorder.style.width, 10)
+  var var1 = 0
+  if ((temp > 360) || (temp < 200)) {
+    var1 = 500;
+  }
+  else if ((temp > 350) || (temp < 210)) {
+    var1 = 300;
+  }
+  else {
+    var1 = -200;
+  }
+
+  renderScore(event.clientX, event.clientY, var1)
+}
+
+
+
+const renderScore = (x, y, var1) => {
+  const TEXTHOLDER = document.createElement('div')
+  TEXTHOLDER.className = 'popupTextHolder'
+  const TEXT = document.createElement('div')
+  TEXT.className = 'popupText'
+  TEXT.innerText = var1
+  TEXTHOLDER.appendChild(TEXT)
+  document.body.appendChild(TEXTHOLDER)
+  gsap
+    .timeline({
+      onComplete: () => TEXTHOLDER.remove(),
+    })
+    .set(TEXTHOLDER, {
+      left: x,
+      top: y,
+      autoAlpha: 1
+    })
+    .to(TEXT, {
+      yPercent: -100,
+      duration: 0.35,
+    })
+    .to(
+      TEXT,
+      {
+        opacity: 0,
+        duration: 0.1,
+      },
+      '>-0.1'
+    )
 }
 
 export default Audience;
